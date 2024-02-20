179787bafdff5242810c3622622b5f0b89756e36
Duong Trong Chi <duongchi68389@gmail.com>
2024-02-21 04:43:42 +0700

    add rest framework

--- core/core/settings.py
+++ core/core/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
-    "engine.apps.EngineConfig"
+    "rest_framework",
+    "engine.apps.EngineConfig",
 ]
 
 MIDDLEWARE = [
