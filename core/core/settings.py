c9c3eda4630b52711fc3709e363446e1692e28c9
Duong Trong Chi <duongchi68389@gmail.com>
2024-03-05 16:11:57 +0700

    add statis file

--- core/core/settings.py
+++ core/core/settings.py
@@ -124,7 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 STATIC_ROOT  = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
